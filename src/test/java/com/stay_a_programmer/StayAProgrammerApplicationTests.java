51561e2cb95312cb33f4b301508624b7749af539
Noroichan <7top.load@gmail.com>
2024-10-13 13:43:42 +0200

    chore: init project

---
+++ src/test/java/com/stay_a_programmer/StayAProgrammerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stay_a_programmer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayAProgrammerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
