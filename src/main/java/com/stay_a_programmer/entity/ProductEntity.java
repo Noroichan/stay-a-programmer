2190afb42ab09f23edb97b0766b29792f5783f52
Noroichan <7top.load@gmail.com>
2024-10-14 16:50:17 +0200

    feat: add product entity

---
+++ src/main/java/com/stay_a_programmer/entity/ProductEntity.java
@@ -0,0 +1,33 @@
+package com.stay_a_programmer.entity;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "products")
+public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, updatable = false)
+    private Long price;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Column(name = "modified_at", nullable = false)
+    private Date modifiedAt;
+
+    @Column(name = "is_deleted", nullable = false, columnDefinition = "BOOLEAN DEFAULT FALSE")
+    private Boolean isDeleted;
+}
