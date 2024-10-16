4ea22351413b03cff1805c7cc84ea59ae2a8b448
Noroichan <7top.load@gmail.com>
2024-10-16 14:37:44 +0200

    feat: add not found exception to service

--- src/main/java/com/stay_a_programmer/exception/NotFoundException.java
+++ src/main/java/com/stay_a_programmer/exception/NotFoundException.java
@@ -1,3 +1,7 @@
 package com.stay_a_programmer.exception;
 
-public class NotFoundException extends RuntimeException {}
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
