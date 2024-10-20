e7bed2c48754e36829beb83d7e6d888f6e9a9f4b
Noroichan <7top.load@gmail.com>
2024-10-20 16:24:28 +0200

    refactor: change record to class for better handling

--- src/main/java/com/stay_a_programmer/dto/CartItemDTO.java
+++ src/main/java/com/stay_a_programmer/dto/CartItemDTO.java
@@ -1,4 +1,42 @@
 package com.stay_a_programmer.dto;
 
-public record CartItemDTO(long id, String name, int price, int amount) {
+public class CartItemDTO {
+    private long id;
+    private String name;
+    private int price;
+    private int amount;
+
+    public CartItemDTO(long id, String name, int price, int amount) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof CartItemDTO)) return false;
+        return this.getId() == ((CartItemDTO) obj).getId();
+    }
 }
