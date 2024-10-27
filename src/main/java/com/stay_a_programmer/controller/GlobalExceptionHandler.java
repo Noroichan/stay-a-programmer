5110cb0ebf9db4629661802ef48fe64dd3cc67d0
Noroichan <7top.load@gmail.com>
2024-10-27 16:26:17 +0100

    feat: missing cookie error handler

--- src/main/java/com/stay_a_programmer/controller/GlobalExceptionHandler.java
+++ src/main/java/com/stay_a_programmer/controller/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.stay_a_programmer.controller;
 import com.stay_a_programmer.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingRequestCookieException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -11,5 +12,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<String> handleNotFoundException(NotFoundException notFoundException) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(notFoundException.getMessage());
+    }
+
+    @ExceptionHandler(MissingRequestCookieException.class)
+    public ResponseEntity<String> handleMissingCookie(MissingRequestCookieException missingRequestCookieException) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(missingRequestCookieException.getCookieName() + " cookie is required!");
     }
 }
