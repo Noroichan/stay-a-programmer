40959af8853321187ff18d484c928f066650f786
Noroichan <7top.load@gmail.com>
2024-11-15 11:57:52 +0100

    fix: path /api

--- src/main/java/com/stay_a_programmer/controller/BaseController.java
+++
@@ -1,9 +0,0 @@
-package com.stay_a_programmer.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api")
-public abstract class BaseController {
-}
